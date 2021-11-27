bed94a77ac2574bc29d0fac381ec7ffae4883e10
Julia_Nakano <88911932+JuliaNakano@users.noreply.github.com>
2021-11-26 22:44:08 -0300

    continuar o exercicio

---
+++ Exercicio_Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //receber um valor do usuario e retornar com a taboada até 10 
+            // receber um valor x 
+
+            Console.Write("Digite um número:");
+            int nx =  Convert.ToInt32(Console.ReadLine());
+
+            if()
+            
+
+        }
+    }
+}
