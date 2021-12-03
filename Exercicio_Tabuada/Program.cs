c7af34ec8dc0e167813eb18f3253bf38f019afbe
Julia_Nakano <88911932+JuliaNakano@users.noreply.github.com>
2021-12-03 20:46:47 -0300

    Solução do exercício Tabuada

--- Exercicio_Tabuada/Program.cs
+++ Exercicio_Tabuada/Program.cs
@@ -6,15 +6,27 @@ namespace Exercicio_Tabuada
     {
         static void Main(string[] args)
         {
-            //receber um valor do usuario e retornar com a taboada até 10 
-            // receber um valor x 
-
-            Console.Write("Digite um número:");
-            int nx =  Convert.ToInt32(Console.ReadLine());
-
-            if()
+            //receber um numero x e mutiplica-lo em fomarto de tabuada (0 a 10)
+            //numero recebido é desconhecido
+            Console.WriteLine ("\nOlá usuario seja bem vindo a calculadora em formato de tabuda" );
+            Console.WriteLine ("Para iniciar....." );
+            Console.WriteLine ("....................");
+            Console.Write("\nDigite um número:.. ");
+            decimal nx = Convert.ToDecimal(Console.ReadLine());
             
-
+            int nmutiplicador = 0;
+            decimal nx1=0;
+        
+            while(nx1 <= 10 * nx)
+            {
+               
+               Console.WriteLine($"{nx} x {nmutiplicador} = {nx1} ");
+             
+               nx1 += nx; 
+              nmutiplicador += 1; 
+            }
+          
         }
+
     }
 }
